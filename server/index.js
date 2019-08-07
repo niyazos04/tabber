c8325fe22d80025fa0ffefbfcdf09aa62efdcc88
niyazos04 <niyaz@incrediblevisibility.com>
2019-08-07 17:35:10 +0530

    new server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/',(req,res) => {
+    res.json({
+        message: 'full stack message dboard'
+    });
+
+});
+
+const port = process.env.PORT || 1234;
+app.listen(port, () => {
+    console.log('listing on ${port}');
+});
+
